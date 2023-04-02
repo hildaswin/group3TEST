038d7e7a916340793c2cd1ef6c8f275d3a2e70c9
hildaswin <94645708+hildaswin@users.noreply.github.com>
2023-04-01 20:26:58 -0500

    first commit

---
+++ Group3API/Program.cs
@@ -0,0 +1,35 @@
+using Group3API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Group3API.Models.Group3_DatabaseContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Group3Database"));
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
